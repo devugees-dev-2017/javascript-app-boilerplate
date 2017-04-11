b369916fd44ea5f969893a06b53ae7bebf9bf183
Raphael Brand <info@raphaelbrand.de>
2017-04-11 21:35:57 +0200

    add onClick callback for all elements with a querySelector

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-function initialize() {
+function initialize(event) {
+  onClick('.numberButton', function (event) {
+    console.log(event.target.textContent)
+  });
 
+  onClick('.numberButton', function (event) {
+    console.log(event.target.className)
+  });
 }
-window.onload = initialize;
+
+
+function onClick(selector = '', callBack = () => { }) {
+  var elementList = Array.from(document.querySelectorAll(selector));
+  elementList.forEach(function (element) {
+    if (element.onclick != null) {
+      var ch = element.onclick;
+      element.addEventListener('click', function () { ch(); callBack(); });
+    }
+    else
+      element.addEventListener('click', callBack);
+  }, this);
+}
+
+document.addEventListener('DOMContentLoaded', initialize, false);
